0449eba2d37c934a8a94bea528d64207796715ec
thibsv <98455585+thibsv@users.noreply.github.com>
2022-02-03 17:32:30 +0100

    Add files via upload

---
+++ perso.java
@@ -0,0 +1,12 @@
+package perso;
+public class perso {
+public perso(String name,int life){
+this.nom = name;
+this.vie = life;
+}
+public String LeNom(){
+return this.nom;
+}
+private String nom;
+private int vie;
+}
