faa1dcf8f42a03b06251f485fafbe726f8c7d1a6
thibsv <98455585+thibsv@users.noreply.github.com>
2022-02-09 09:20:03 +0100

    Add files via upload

---
+++ jeu_perso.java
@@ -0,0 +1,7 @@
+import perso.*;
+public class jeu_perso{
+public static void main(String arg []){
+perso sorcier = new perso("nico",80);
+system.out.println("nom du perso : " + sorcier.LeNom());
+}
+}
