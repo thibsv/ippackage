29ce7296ed508c83a1d2de894556eb908f67a67d
thibsv <98455585+thibsv@users.noreply.github.com>
2022-02-11 10:24:51 +0100

    Update jeu_perso.java

--- jeu_perso.java
+++ jeu_perso.java
@@ -1,7 +1,10 @@
 import perso.*;
 public class jeu_perso{
 public static void main(String arg []){
-perso sorcier = new perso("nico",80);
-system.out.println("nom du perso : " + sorcier.LeNom());
+perso sorcier = new perso("pere fouras",80);
+perso sorcier = new perso("four",300);
+system.out.println(il etait une fois dans un lointain royaume, le pere fouras se preparant un delicieu gateau.);
+system.out.println(malheusement il se tappa son pied dans le four en cuisinant et celui ci pris vit et tenta de cuire le pere fouras.);
+system.out.println(le four attaque le pere fouras et 
 }
 }
