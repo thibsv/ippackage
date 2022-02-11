c1f64dbf83609a2095f476ce04561a91f1883e88
thibsv <98455585+thibsv@users.noreply.github.com>
2022-02-11 10:24:44 +0100

    Update perso.java

--- perso.java
+++ perso.java
@@ -1,5 +1,7 @@
 package perso;
 public class perso {
+ private String nom;
+private int vie;
 public perso(String name,int life){
 this.nom = name;
 this.vie = life;
@@ -7,6 +9,11 @@ this.vie = life;
 public String LeNom(){
 return this.nom;
 }
-private String nom;
-private int vie;
+public void damage(Personnage cible, int degats){
+		cible.vie -= degats;
+		System.out.println(this.LeNom() + " attaque " + cible.LeNom() + " qui perd " + degats + " Points de vie");
+
+	}
+
+
 }
